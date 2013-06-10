1783aca9c8ba8ce76947a30176c42d3671dd1c91
RogerE <roger@webfokus.no>
2013-06-10 16:06:08 +0200

    Added gem files generated by bundler

---
+++ lib/jquery/mobile/rails/assets.rb
@@ -0,0 +1,11 @@
+require "jquery/mobile/rails/assets/version"
+
+module Jquery
+  module Mobile
+    module Rails
+      module Assets
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/jquery/mobile/rails/assets/version.rb
@@ -0,0 +1,9 @@
+module Jquery
+  module Mobile
+    module Rails
+      module Assets
+        VERSION = "0.0.1"
+      end
+    end
+  end
+end
